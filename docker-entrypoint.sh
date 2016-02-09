67dfe7454d731027054c7db76bb81ecfdd87ccac
Bakudankun <bakudankun@gmail.com>
2016-02-09 13:29:03 +0900

    Add function to show usage

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,12 +1,26 @@
 #!/bin/sh
 
-OPT=`getopt -o "" -l "tarball,zipball" -- "$@"`
+show-usage() {
+	cat <<EOT
+Usage:
+ docker run [--rm] -v /path/to/outdir:/out bakudankun/ricty-generator
+ docker run [--rm] bakudankun/ricty-generator --tarball > Ricty.tar.gz
+ docker run [--rm] bakudankun/ricty-generator --zipball > Ricty.zip
+ docker run [--rm] bakudankun/ricty-generator [-h|--help]
+EOT
+}
+
+OPT=`getopt -o "h" -l "tarball,zipball,help" -- "$@"`
+if [ $? != 0 ]; then show-usage; exit 1; fi
+
 set -- $OPT
 
 while [ $# -gt 0 ]; do
 	case $1 in
 		--tarball) tarball=true ;;
 		--zipball) zipball=true ;;
+		-h | --help) show-usage; exit 0 ;;
+		*) show-usage; exit 1 ;;
 	esac
 	shift
 done
@@ -16,7 +30,18 @@ cd /Ricty-${RICTY_VERSION}
 if [ ! -e Ricty-Regular.ttf ]; then
 	if [ ! "$tarball" -a ! "$zipball" ]; then
 		./ricty_generator.sh auto
-		./misc/os2version_reviser.sh Ricty*.ttf
+			if [ ! -e Ricty-Regular.ttf ]; then
+				echo 'Failed to generate Ricty. exitting...'
+				exit 1;
+			else
+				echo 'Now revise fonts for OS/2 (it may takes a little time).'
+				./misc/os2version_reviser.sh Ricty*.ttf
+				if [ $? = 0 ]; then
+					echo 'Complete!'
+				else
+					echo 'Failed to revise fonts. The output fonts may have wide spaces.'
+				fi
+			fi
 	else
 		./ricty_generator.sh auto >/dev/null 2>&1
 		./misc/os2version_reviser.sh Ricty*.ttf >/dev/null 2>&1
@@ -26,6 +51,7 @@ fi
 if [ -e Ricty-Regular.ttf ]; then
 	if [ ! "$tarball" -a ! "$zipball" ]; then
 		cp -r Ricty*.ttf LICENSE README.md /out
+		echo 'Copied font files. Check the output dir.'
 	elif [ "$tarball" -o "$zipball" ]; then
 		outdir="Ricty-v${RICTY_VERSION}"
 		mkdir $outdir
